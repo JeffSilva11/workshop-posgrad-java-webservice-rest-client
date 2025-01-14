bf9aadadd5300dcdc7a3bfdc87b68474e1dd2bdf
Jefferson Silva <fj_adm@hotmail.com>
2025-01-13 23:29:05 -0300

    UN05-07 - Criada: server.keystore, Alterada: main.java para disponibilizar  configuração para acesso via SSL e LOGIN com usuário e senha

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
-import javax.ws.rs.client.WebTarget;
+//import javax.ws.rs.client.Invocation.Builder;
+//import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.SslConfigurator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +16,25 @@ public class Main {
 //        Builder request = targetLivro.request(MediaType.APPLICATION_XML);
 //        Livros livros = request.get(Livros.class);
     	
-    	Livros livros = ClientBuilder.newClient()
-    	        .target("http://localhost:8080/livraria-virtual")
+    	SslConfigurator config = SslConfigurator
+    	        .newInstance()
+    	        .trustStoreFile("server.keystore")
+    	        .trustStorePassword("livraria");
+
+    	SSLContext context = config.createSSLContext();
+   	
+    	//Client client = ClientBuilder.newClient();
+    	Client client = ClientBuilder.newBuilder()
+    	        .sslContext(context)
+    	        .build();
+    	
+    	HttpAuthenticationFeature auth = HttpAuthenticationFeature.basic("admin", "password");
+    	client.register(auth);    	
+    	
+    	//Livros livros = ClientBuilder.newClient()
+    	Livros livros = client
+    	        //.target("http://localhost:8080/livraria-virtual")
+    	        .target("https://localhost:8443/livraria-virtual")
     	        .path("livro")
     	        .request(MediaType.APPLICATION_XML)
     	        .get(Livros.class);
